61e3f33183e7ab0c01b54d1de482d394b0aae67e
talafarael <127351345+talafarael@users.noreply.github.com>
2024-07-26 12:35:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const telegramApi=require('node-telegram-bot-api')
 
 const token='7218356256:AAE-8mBqDMnzso1WEQLTROnSv49a9WbXc4w'
 const bot=new telegramApi(token,{polling:true})
-bot.setMyCommands([
-    {command:'/start',description:'start'}
+const chats={}
+const start=()=>{
+   bot.setMyCommands([
+    {command:'/start',description:'start'},
+    {command:'/game',description:'game'}
 ])
 bot.on('message',async msg=>{
     const text=msg.text
     const chatId=msg.chat.id
-    bot.sendMessage(chatId,`hello ${text}`)
-})
+ if(text=='/start'){
+    return bot.sendMessage(chatId,'start')
+}
+if(text=='/game'){
+    await bot.sendMessage(chatId,'сеййчас загадаю игру')
+    const randomNumber=Math.floor(Math.random()*100)
+    chats[chatId]=randomNumber
+    return bot.sendMessage(chatId,'отгадывай')
+}
+return bot.sendMessage(chatId,'i dont understand')
+}) 
+
+}
+start()
